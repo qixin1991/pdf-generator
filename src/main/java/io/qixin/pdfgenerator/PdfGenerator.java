a163dc90d322542fe942662352ef4d56173ef07e
无忌 <me@qixin.io>
2018-01-10 13:14:08 +0800

    ftl to html string

--- src/main/java/io/qixin/pdfgenerator/PdfGenerator.java
+++ src/main/java/io/qixin/pdfgenerator/PdfGenerator.java
@@ -9,17 +9,22 @@ import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.font.FontProvider;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.StringWriter;
 
 /**
  * Pdf-html 转换
  */
 public class PdfGenerator {
+    public static Configuration configuration = new Configuration(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS);
     public static final String FONT_PATH = "src/main/resources/fonts";
     /**
-     *
+     * html -> pdf
      * @param htmlStr html string.
      * @param os output stream, for example: HttpServletResponse.
      * @param pageSize print page size.
@@ -43,5 +48,25 @@ public class PdfGenerator {
         properties.setCreateAcroForm(true);
         HtmlConverter.convertToPdf(htmlStr,pdfDoc, properties);
         pdfDoc.close();
+    }
+
+    /**
+     * freemarker template -> html string.
+     * @param param Object param
+     * @param ftlFilePath freemarker template file path. example: "src/main/resources/templates/hello.ftl"
+     * @return html string.
+     * @throws IOException
+     * @throws TemplateException
+     */
+    public static String generateStringFromFtl(Object param,String ftlFilePath) throws IOException, TemplateException {
+        configuration.setDefaultEncoding("UTF-8");
+        Template template = configuration.getTemplate(ftlFilePath);
+
+        StringWriter stringWriter = new StringWriter();
+        template.process(param,stringWriter);
+        String htmlStr = stringWriter.toString();
+        stringWriter.flush();
+        stringWriter.close();
+        return htmlStr;
     }
 }
